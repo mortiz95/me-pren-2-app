26f9cf95804209cd950b754e864ef5751c391270
Matias Ortiz <matiaaasortiz52@gmail.com>
2024-01-19 18:48:06 +0100

    removed ExploreContainer

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
+      <ion-tab-bar>
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon aria-hidden="true" :icon="triangle" />
           <ion-label>Tab 1</ion-label>
